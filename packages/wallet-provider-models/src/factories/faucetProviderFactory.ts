fc4a8a7be13db97d978d767255f686b95c519a50
Martyn Janes <martyn@obany.com>
2024-04-05 12:22:19 +0200

    Update dependencies (#4)
    * Update dependencies
    
    * Linting

--- packages/wallet-provider-models/src/factories/faucetProviderFactory.ts
+++ packages/wallet-provider-models/src/factories/faucetProviderFactory.ts
@@ -1,13 +1,10 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import { FactoryInstance } from "@gtsc/core";
+import { Factory } from "@gtsc/core";
 import type { IFaucetProvider } from "../models/IFaucetProvider";
 
 /**
  * Factory for creating faucets.
  */
-export class FaucetProviderFactory {
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	public static readonly Instance: FactoryInstance<IFaucetProvider> =
-		new FactoryInstance<IFaucetProvider>("faucet");
-}
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const FaucetProviderFactory = new Factory<IFaucetProvider>("faucet");
--- packages/wallet-provider-models/src/factories/walletProviderFactory.ts
+++ packages/wallet-provider-models/src/factories/walletProviderFactory.ts
@@ -1,13 +1,10 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import { FactoryInstance } from "@gtsc/core";
+import { Factory } from "@gtsc/core";
 import type { IWalletProvider } from "../models/IWalletProvider";
 
 /**
  * Factory for creating wallet providers.
  */
-export class WalletProviderFactory {
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	public static readonly Instance: FactoryInstance<IWalletProvider> =
-		new FactoryInstance<IWalletProvider>("walletProvider");
-}
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const WalletProviderFactory = new Factory<IWalletProvider>("walletProvider");
