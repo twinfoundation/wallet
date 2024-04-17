6e9ee1a061dadaf4ffb1ec5d3d8da399b45f1632
Martyn Janes <martyn@obany.com>
2024-04-17 13:11:12 +0200

    Change order of constructor dependencies (#8)

--- packages/wallet-provider-iota/tests/iotaWalletProvider.spec.ts
+++ packages/wallet-provider-iota/tests/iotaWalletProvider.spec.ts
@@ -18,12 +18,50 @@ describe("IotaWalletProvider", () => {
 		await initTestWallet();
 	});
 
+	test("can fail to construct a wallet with no dependencies", () => {
+		expect(
+			() =>
+				new IotaWalletProvider(
+					undefined as unknown as { vaultProvider: IVaultProvider },
+					undefined as unknown as IIotaWalletProviderConfig
+				)
+		).toThrow(
+			expect.objectContaining({
+				name: "GuardError",
+				message: "guard.objectUndefined",
+				properties: {
+					property: "dependencies",
+					value: "undefined"
+				}
+			})
+		);
+	});
+
+	test("can fail to construct a wallet with no vault", () => {
+		expect(
+			() =>
+				new IotaWalletProvider(
+					{ vaultProvider: undefined as unknown as IVaultProvider },
+					undefined as unknown as IIotaWalletProviderConfig
+				)
+		).toThrow(
+			expect.objectContaining({
+				name: "GuardError",
+				message: "guard.objectUndefined",
+				properties: {
+					property: "dependencies.vaultProvider",
+					value: "undefined"
+				}
+			})
+		);
+	});
+
 	test("can fail to construct a wallet with no config", () => {
 		expect(
 			() =>
 				new IotaWalletProvider(
-					undefined as unknown as IIotaWalletProviderConfig,
-					undefined as unknown as IVaultProvider
+					{ vaultProvider: {} as unknown as IVaultProvider },
+					undefined as unknown as IIotaWalletProviderConfig
 				)
 		).toThrow(
 			expect.objectContaining({
@@ -41,8 +79,8 @@ describe("IotaWalletProvider", () => {
 		expect(
 			() =>
 				new IotaWalletProvider(
-					{} as unknown as IIotaWalletProviderConfig,
-					undefined as unknown as IVaultProvider
+					{ vaultProvider: {} as unknown as IVaultProvider },
+					{} as unknown as IIotaWalletProviderConfig
 				)
 		).toThrow(
 			expect.objectContaining({
@@ -59,10 +97,9 @@ describe("IotaWalletProvider", () => {
 	test("can fail to construct a wallet with no wallet mnemonic id", () => {
 		expect(
 			() =>
-				new IotaWalletProvider(
-					{ clientOptions: {} } as unknown as IIotaWalletProviderConfig,
-					undefined as unknown as IVaultProvider
-				)
+				new IotaWalletProvider({ vaultProvider: {} as unknown as IVaultProvider }, {
+					clientOptions: {}
+				} as unknown as IIotaWalletProviderConfig)
 		).toThrow(
 			expect.objectContaining({
 				name: "GuardError",
@@ -75,35 +112,15 @@ describe("IotaWalletProvider", () => {
 		);
 	});
 
-	test("can fail to construct a wallet with no vault", () => {
-		expect(
-			() =>
-				new IotaWalletProvider(
-					{
-						clientOptions: TEST_CLIENT_OPTIONS,
-						walletMnemonicId: TEST_MNEMONIC_ID
-					},
-					undefined as unknown as IVaultProvider
-				)
-		).toThrow(
-			expect.objectContaining({
-				name: "GuardError",
-				message: "guard.objectUndefined",
-				properties: {
-					property: "vaultProvider",
-					value: "undefined"
-				}
-			})
-		);
-	});
-
 	test("can construct a wallet with details", () => {
 		const faucet = new IotaWalletProvider(
 			{
+				vaultProvider: TEST_VAULT
+			},
+			{
 				clientOptions: TEST_CLIENT_OPTIONS,
 				walletMnemonicId: TEST_MNEMONIC_ID
-			},
-			TEST_VAULT
+			}
 		);
 		expect(faucet).toBeDefined();
 	});
@@ -111,10 +128,12 @@ describe("IotaWalletProvider", () => {
 	test("can fail to ensure a balance on an address with no faucet available", async () => {
 		const wallet = new IotaWalletProvider(
 			{
+				vaultProvider: TEST_VAULT
+			},
+			{
 				clientOptions: TEST_CLIENT_OPTIONS,
 				walletMnemonicId: TEST_MNEMONIC_ID
-			},
-			TEST_VAULT
+			}
 		);
 
 		const ensured = await wallet.ensureBalance(TEST_CONTEXT, TEST_ADDRESS_BECH32, 1000000000n);
@@ -124,14 +143,16 @@ describe("IotaWalletProvider", () => {
 	test("can ensure a balance on an address", async () => {
 		const wallet = new IotaWalletProvider(
 			{
+				vaultProvider: TEST_VAULT,
+				faucetProvider: new IotaFaucetProvider({
+					clientOptions: TEST_CLIENT_OPTIONS,
+					endpoint: process.env.TEST_FAUCET_ENDPOINT ?? ""
+				})
+			},
+			{
 				clientOptions: TEST_CLIENT_OPTIONS,
 				walletMnemonicId: TEST_MNEMONIC_ID
-			},
-			TEST_VAULT,
-			new IotaFaucetProvider({
-				clientOptions: TEST_CLIENT_OPTIONS,
-				endpoint: process.env.TEST_FAUCET_ENDPOINT ?? ""
-			})
+			}
 		);
 
 		const ensured = await wallet.ensureBalance(TEST_CONTEXT, TEST_ADDRESS_BECH32, 1000000000n);
@@ -142,10 +163,12 @@ describe("IotaWalletProvider", () => {
 	test("can get a balance for an address", async () => {
 		const wallet = new IotaWalletProvider(
 			{
+				vaultProvider: TEST_VAULT
+			},
+			{
 				clientOptions: TEST_CLIENT_OPTIONS,
 				walletMnemonicId: TEST_MNEMONIC_ID
-			},
-			TEST_VAULT
+			}
 		);
 
 		const balance = await wallet.getBalance(TEST_CONTEXT, TEST_ADDRESS_BECH32);
@@ -157,10 +180,12 @@ describe("IotaWalletProvider", () => {
 	test("can get storage costs for an address", async () => {
 		const wallet = new IotaWalletProvider(
 			{
+				vaultProvider: TEST_VAULT
+			},
+			{
 				clientOptions: TEST_CLIENT_OPTIONS,
 				walletMnemonicId: TEST_MNEMONIC_ID
-			},
-			TEST_VAULT
+			}
 		);
 
 		const storageCosts = await wallet.getStorageCosts(TEST_CONTEXT, TEST_ADDRESS_BECH32);
